4ede5a33ba8701d5c6a60c941d31a7a80bf4d8d7
Tomio <temujin142857@gmail.com>
2026-03-24 21:56:05 -0400

    setting up he procedures from the assignment instructions and moving graph logic to it's own file

---
+++ graph.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var STATES = [...]string{"DOWNTOWN", "ASLEEP", "VILLAGE", "DONE"}
+var MESSAGE_CATEGORIES = [...]string{"FIND_SMALLEST_FRINGE_EDGE", "SMALLEST_FRINGE_EDGE_FOUND", "MERE_REQUEST", "MERGE_REQUESTED", "GET_ABSORBED", "WE_ABSORBED_THEM", "CITY_CHECK"}
+var SUB_STATES = [...]string{"WAITING_TO_REPLY"}
+
+type edgePath struct {
+	edges []int
+}
+
+type Message struct {
+	catagory        string
+	sender          int
+	level           int
+	city            int
+	callbackPath    edgePath
+	destinationPath edgePath
+}
+
+type Node struct {
+	name      int
+	level     int
+	city      int
+	parent    int
+	children  []int
+	edges     []Vertex
+	neighbors map[int]int
+	state     string
+	initiator bool
+}
+
+type Vertex struct {
+	name    int
+	node1   int
+	node2   int
+	channel chan Message
+}
+
+func instructions(node *Node) {
+	if node.initiator {
+		start(node)
+	}
+
+	cases := make([]reflect.SelectCase, len(node.edges))
+	for i, ch := range node.edges {
+		cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
+	}
+
+	for true {
+
+		chosen, value, ok := reflect.Select(cases)
+		if ok {
+			panic("unexpected channel closed")
+		}
+		senderIndex := node.edges[chosen]
+		message := value.String()
+
+		switch {
+		case message == "" && node.state == "":
+			fmt.Println("")
+		case message == "a" && node.state == "":
+
+		case message == "complete":
+
+		}
+		if node.state == "done" {
+			break
+		}
+	}
+	fmt.Println("done")
+
+}
+
+func findSmallestExternalEdge(node *Node) int { return 0 }
+
+func start(node *Node) {
+	node.state = "Downtown"
+	path := findSmallestExternalEdge(node)
+
+}
+
+func (s *edgePath) Push(item int) {
+	s.edges = append(s.edges, item)
+}
+
+func (s *edgePath) Pop() (int, bool) {
+	if len(s.edges) == 0 {
+		return 0, false
+	}
+	item := s.edges[len(s.edges)-1]
+	s.edges = s.edges[:len(s.edges)-1]
+	return item, true
+}
+
+func (s *edgePath) IsEmpty() bool {
+	return len(s.edges) == 0
+}
--- main.go
+++ main.go
@@ -3,57 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
-	"reflect"
+	"math"
 	"sync"
 	//"gg"
 )
 
-type edgePath struct {
-	edges []int
-}
-
-type Message struct {
-	catagory        string
-	sender          int
-	level           int
-	city            int
-	callbackPath    edgePath
-	destinationPath edgePath
-}
-
-type Node struct {
-	name      int
-	level     int
-	city      int
-	parent    int
-	children  []int
-	edges     []Vertex
-	neighbors map[int]int
-	state     string
-	initiator bool
-}
-
-type Vertex struct {
-	name    int
-	node1   int
-	node2   int
-	channel chan Message
-}
-
-var nodes map[int]Node = make(map[int]Node)
+//var nodes map[int]Node = make(map[int]Node)
 
 var (
-	fileFlag     bool
+	procedureNum int
 	filePath     string
 	withWeight   bool
-	initiatorNum int
+	nodeNum      int
 )
 
 func init() {
-	flag.BoolVar(&fileFlag, "fileFlag", false, "Wether making graph from file or rand")
+	flag.IntVar(&procedureNum, "procedureNum", 0, "0 for file, 1 for procedure1, 2 for procedure2")
 	flag.StringVar(&filePath, "filePath", "default", "name of file with graph")
 	flag.BoolVar(&withWeight, "withWeight", false, "Wether the file has weights")
-	flag.IntVar(&initiatorNum, "initiatorNum", 1, "How many itiators")
+	flag.IntVar(&nodeNum, "nodeNum", 2, "How many nodes")
 }
 
 func main() {
@@ -64,81 +32,81 @@ func main() {
 	}()
 
 	flag.Parse()
-	fmt.Println("fileFlag:", fileFlag, ", filePath:", filePath, ", withWeight:", withWeight)
+	fmt.Println("procedureNum:", procedureNum, ", filePath:", filePath, ", withWeight:", withWeight)
 
-	var wg sync.WaitGroup
-
-	if fileFlag {
-		fileSetup(filePath, withWeight, initiatorNum)
-	} else {
-		randomSetup(5, 7, 2)
+	switch procedureNum {
+	case 0:
+		fileSetup(filePath, withWeight, nodeNum)
+		break
+	case 1:
+		procedure1(nodeNum)
+		break
+	case 2:
+		procedure2()
 	}
-	fmt.Println(nodes)
-	VisualizeGraph(nodes, "network")
 
+}
+
+func procedure1(n int) [4]int {
+	var averageComplexities [4]int
+	m := n
+	for i := 0; i < 4; i++ {
+		switch i {
+		case 0:
+			m = n
+			break
+		case 1:
+			m = n * int(math.Round(math.Log2(float64(n))))
+			break
+		case 2:
+			m = n * int(math.Sqrt(float64(n)))
+			break
+		case 3:
+			m = n ^ 2
+			break
+		}
+		totalComplexity := 0
+		for j := 0; j < 1000; j++ {
+			var nodes map[int]Node = make(map[int]Node)
+			generateRandomGraph(n, m, n, nodes)
+			complexity := runAlgorithm(nodes)
+			totalComplexity += complexity
+		}
+		averageComplexities[i] = totalComplexity / 1000
+	}
+	return averageComplexities
+}
+
+func procedure2() [4]int {
+	var averageComplexities [4]int
+	edgeGrowth := func(n int) int {
+		return 3 * n
+	}
+	var nValues = [...]int{20, 30, 40, 60, 80, 100}
+
+	for i := 0; i < 4; i++ {
+		n := nValues[i]
+		m := edgeGrowth(n)
+		totalComplexity := 0
+		for j := 0; j < 1000; j++ {
+			var nodes map[int]Node = make(map[int]Node)
+			generateRandomGraph(n, m, n, nodes)
+			complexity := runAlgorithm(nodes)
+			totalComplexity += complexity
+		}
+		averageComplexities[i] = totalComplexity / 1000
+	}
+	return averageComplexities
+}
+
+func runAlgorithm(nodes map[int]Node) int {
+	var wg sync.WaitGroup
 	for _, node := range nodes {
 		wg.Go(func() { instructions(&node) })
 	}
 
 	wg.Wait()
-}
 
-func instructions(node *Node) {
-	if node.initiator {
-		start(node)
-	}
-
-	cases := make([]reflect.SelectCase, len(node.edges))
-	for i, ch := range node.edges {
-		cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
-	}
-
-	for true {
-
-		chosen, value, ok := reflect.Select(cases)
-		if ok {
-			panic("unexpected channel closed")
-		}
-		senderIndex := node.edges[chosen]
-		message := value.String()
-
-		switch {
-		case message == "" && node.state == "":
-			fmt.Println("")
-		case message == "a" && node.state == "":
-
-		case message == "complete":
-
-		}
-		if node.state == "done" {
-			break
-		}
-	}
-	fmt.Println("done")
-
-}
-
-func start(node *Node) {
-	node.state = "Downtown"
-	path := findSmallestExternalEdge(node)
-
-}
-
-func findSmallestExternalEdge(node *Node) int { return 0 }
-
-func (s *edgePath) Push(item int) {
-	s.edges = append(s.edges, item)
-}
-
-func (s *edgePath) Pop() (int, bool) {
-	if len(s.edges) == 0 {
-		return 0, false
-	}
-	item := s.edges[len(s.edges)-1]
-	s.edges = s.edges[:len(s.edges)-1]
-	return item, true
-}
-
-func (s *edgePath) IsEmpty() bool {
-	return len(s.edges) == 0
+	VisualizeGraph(nodes, "network")
+	return 0
 }
