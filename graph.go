648fc4a8a0994c36515de0f93fc0bd3de802fbd3
Tomio <temujin142857@gmail.com>
2026-04-05 23:02:00 -0400

    implementing instructions

--- graph.go
+++ graph.go
@@ -15,20 +15,28 @@ type Message struct {
 	city            int
 	callbackPath    edgePath
 	destinationPath edgePath
+	answer          string
+	payload         int
 }
 
 // parent and children are indexes for the edges array
 type Node struct {
-	name      int
-	level     int
-	city      int
-	parent    int
-	children  []int
-	edges     []Vertex
+	name  int
+	level int
+	city  int
+	//parent and children should be edge indixes
+	parent   int
+	children []int
+	edges    []Vertex
+	//only used in setup
 	neighbors map[int]int
-	state     string
-	initiator bool
-	inbox     chan Message
+	//maps vertex to a 1 if internal, 2 if a child
+	internalNeighbors      map[int]int
+	mySmallestInternalEdge int
+	state                  string
+	substate               string
+	initiator              bool
+	inbox                  chan Message
 }
 
 type Vertex struct {
@@ -41,15 +49,32 @@ func (s *edgePath) Push(item int) {
 	s.edges = append(s.edges, item)
 }
 
-func (s *edgePath) Pop() (int, bool) {
+func (s *edgePath) Pop() int {
 	if len(s.edges) == 0 {
-		return 0, false
+		return -1
 	}
 	item := s.edges[len(s.edges)-1]
 	s.edges = s.edges[:len(s.edges)-1]
-	return item, true
+	return item
+}
+
+func (s *edgePath) Peek() int {
+	if len(s.edges) == 0 {
+		return -1
+	}
+	item := s.edges[len(s.edges)-1]
+	return item
 }
 
 func (s *edgePath) IsEmpty() bool {
 	return len(s.edges) == 0
+}
+
+func (n *Node) removeChild(target int) {
+	for i, v := range n.children {
+		if v == target {
+			n.children = append(n.children[:i], n.children[i+1:]...)
+			return
+		}
+	}
 }
--- megamergerInstructions.go
+++ megamergerInstructions.go
@@ -10,16 +10,18 @@ func instructions(node *Node, complexity *int) {
 	for true {
 
 		message := <-node.inbox
-		//senderIndex := message.sender
 
-		switch {
-		case message.catagory == "" && node.state == "":
-			fmt.Println("")
-		case message.catagory == "a" && node.state == "":
-
-		case message.catagory == "complete":
-
+		switch node.state {
+		case "Downtown":
+			downTownInstructions(node, &message, complexity)
+		case "Village":
+			villageInstructions(node, &message, complexity)
+		case "Asleep":
+			downTownInstructions(node, &message, complexity)
+		case "Done":
+			downTownInstructions(node, &message, complexity)
 		}
+
 		if node.state == "done" {
 			break
 		}
@@ -30,13 +32,168 @@ func instructions(node *Node, complexity *int) {
 func findSmallestExternalEdge(node *Node) int { return 0 }
 
 // target is the index of the edge in node.edges
-func sendMessage(node *Node, target int, complexity *int) {
+func sendMessage(node *Node, target int, message *Message, complexity *int) {
 	*complexity++
 
+}
+
+func broadcast(node *Node, message Message, compexity *int) {
+
+}
+
+func queryNonChildren(node *Node, message Message, compexity *int) {
+	for i := 0; i < len(node.neighbors); i++ {
+		if _, ok := node.internalNeighbors[i]; !ok {
+			outMessage := Message{catagory: "cityCheck", sender: node.internalNeighbors[i], level: node.level, city: node.city}
+			sendMessage(node, node.internalNeighbors[i], &outMessage, compexity)
+			return
+		}
+	}
 }
 
 func start(node *Node) {
 	node.state = "Downtown"
 	//path := findSmallestExternalEdge(node)
 
+}
+
+func villageInstructions(node *Node, message *Message, complexity *int) {
+	switch message.catagory {
+	case "findSmallestFringeEdge":
+		callback := edgePath{edges: append(message.callbackPath.edges, message.sender)}
+		outMessage := Message{catagory: "findSmallestFringeEdge", sender: message.sender, level: node.level, city: node.city, callbackPath: callback}
+		broadcast(node, outMessage, complexity)
+		outMessage2 := Message{catagory: "cityCheck", sender: message.sender, level: node.level, city: node.city, callbackPath: callback}
+		queryNonChildren(node, outMessage2, complexity)
+
+	case "smallestFringeEdgeFound":
+		if message.sender == node.parent {
+			panic("village recieved smallest fringe edge from parent")
+		}
+		if message.payload > node.mySmallestInternalEdge {
+			message.payload = node.mySmallestInternalEdge
+			message.callbackPath = edgePath{edges: []int{node.parent}}
+		} else {
+			message.callbackPath.edges = append(message.callbackPath.edges, node.parent)
+		}
+		sendMessage(node, node.parent, message, complexity)
+
+	case "mergeRequest":
+		outMessage := Message{catagory: "getAbsorbed", sender: message.sender, level: node.level, city: node.city, destinationPath: message.callbackPath}
+		if node.level > message.level {
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+			node.children = append(node.children, message.sender)
+			node.internalNeighbors[message.sender] = 2
+		} else {
+			node.substate = "waitingToReply"
+
+		}
+		//note consider if if b and c both make a merge request to me
+		//use array to store pending request
+		//just need to store level, and sender
+
+	case "mergeAccepted":
+		outMessage := Message{catagory: "mergeAccepted", sender: message.sender, level: message.level, city: message.city}
+		broadcast(node, outMessage, complexity)
+		if message.city != node.city {
+			node.city = message.city
+			node.level = message.level
+			node.state = "Village"
+			node.parent = message.callbackPath.Peek()
+			node.removeChild(message.callbackPath.Peek())
+		}
+
+	case "getAbsorbed":
+		if message.level <= node.level {
+			panic("downtown recieved asborb message with smaller level")
+		}
+		outMessage := Message{catagory: "getAbsorbed", sender: message.sender, level: message.level, city: message.city}
+		broadcast(node, outMessage, complexity)
+		node.city = message.city
+		node.level = message.level
+		node.state = "Village"
+		node.parent = message.callbackPath.Peek()
+		node.removeChild(message.callbackPath.Peek())
+
+	case "cityCheck":
+		if message.city == node.city {
+			outMessage := Message{catagory: "cityCheckReply", sender: message.sender, level: node.level, city: node.city, answer: "internal"}
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+		} else if node.level >= message.level {
+			outMessage := Message{catagory: "cityCheckReply", sender: message.sender, level: node.level, city: node.city, answer: "external"}
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+		} else {
+			node.substate = "WatingToReply"
+		}
+
+	case "cityCheckReply":
+		//need to write
+
+	case "terminationBroadcast":
+		panic("downtown recieved termination broadcast")
+
+	}
+}
+
+func downTownInstructions(node *Node, message *Message, complexity *int) {
+	switch message.catagory {
+	case "findSmallestFringeEdge":
+		panic("Downtown recieved find smallest fringe edge")
+
+	case "smallestFringeEdgeFound":
+		callback := edgePath{edges: []int{message.callbackPath.Peek()}}
+		outMessage := Message{catagory: "mergeRequest", sender: message.sender, level: node.level, city: node.city, callbackPath: callback, destinationPath: message.callbackPath}
+		sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+
+	case "mergeRequest":
+		outMessage := Message{catagory: "getAbsorbed", sender: message.sender, level: node.level, city: node.city, destinationPath: message.callbackPath}
+		if node.level > message.level {
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+			node.children = append(node.children, message.sender)
+			node.internalNeighbors[message.sender] = 2
+		} else {
+			node.substate = "waitingToReply"
+		}
+
+	case "mergeAccepted":
+		outMessage := Message{catagory: "mergeAccepted", sender: message.sender, level: message.level, city: message.city}
+		broadcast(node, outMessage, complexity)
+		if message.city != node.city {
+			node.city = message.city
+			node.level = message.level
+			node.state = "Village"
+			node.parent = message.callbackPath.Peek()
+			node.removeChild(message.callbackPath.Peek())
+		}
+
+	case "getAbsorbed":
+		if message.level <= node.level {
+			panic("downtown recieved asborb message with smaller level")
+		}
+		outMessage := Message{catagory: "getAbsorbed", sender: message.sender, level: message.level, city: message.city}
+		broadcast(node, outMessage, complexity)
+		node.city = message.city
+		node.level = message.level
+		node.state = "Village"
+		node.parent = message.callbackPath.Peek()
+		node.removeChild(message.callbackPath.Peek())
+
+	case "cityCheck":
+		if message.city == node.city {
+			outMessage := Message{catagory: "cityCheckReply", sender: message.sender, level: node.level, city: node.city, answer: "internal"}
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+		} else if node.level >= message.level {
+			outMessage := Message{catagory: "cityCheckReply", sender: message.sender, level: node.level, city: node.city, answer: "external"}
+			sendMessage(node, outMessage.destinationPath.Pop(), &outMessage, complexity)
+		} else {
+			node.substate = "WatingToReply"
+		}
+
+	case "cityCheckReply":
+		//need to write
+
+	case "terminationBroadcast":
+		panic("downtown recieved termination broadcast")
+
+	}
 }
